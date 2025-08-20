3c2c5fc04b3856a4058e140dd916427d6da8b26e
rezasetyawan <setyawanreza960@gmail.com>
2025-08-20 11:57:32 +0700

    update: clear commented code

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -1,28 +1,3 @@
-// import { Button } from "@/components/ui/button";
-// import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
-// import { useTheme } from "./theme-context";
-
-// export function ModeToggle() {
-//   const { theme, setTheme } = useTheme();
-
-//   return (
-//     <Button
-//       variant="ghost"
-//       type="button"
-//       size="icon"
-//       className="px-2"
-//       onClick={() => {
-//         console.log("Toggling theme", theme);
-//         setTheme(theme === "dark" ? "light" : "dark")
-//       }}
-//     >
-//       <SunIcon className="h-[1.2rem] w-[1.2rem] text-neutral-800 dark:hidden dark:text-neutral-200" />
-//       <MoonIcon className="hidden h-[1.2rem] w-[1.2rem] text-neutral-800 dark:block dark:text-neutral-200" />
-//     </Button>
-//   );
-// }
-
-
 "use client";
 
 import { useEffect, useState } from "react";
